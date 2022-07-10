a9207337a1700a10a161524fee1cdcd4ef70959b
Javier Humberto García Gómez <javieron.garcia@gmail.com>
2022-07-10 22:34:05 +0200

    section 13 - project

---
+++ s12-project/app20220709/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit, Input } from "@angular/core";
+import { RecipeService } from "src/app/services/recipe.service";
+
+import { Recipe } from "../recipe.model";
+import { ActivatedRoute, Router, Params } from "@angular/router";
+
+@Component({
+  selector: "app-recipe-detail",
+  templateUrl: "./recipe-detail.component.html",
+  styleUrls: ["./recipe-detail.component.css"],
+})
+export class RecipeDetailComponent implements OnInit {
+  // @Input() recipe: Recipe;
+  recipe: Recipe;
+  id: number;
+
+  constructor(
+    private recipeService: RecipeService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    this.route.params.subscribe((params: Params) => {
+      this.id = +params["id"];
+      this.recipe = this.recipeService.getRecipe(this.id);
+    });
+  }
+
+  onAddToShoppingList() {
+    this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+  }
+
+  onEditRecipe() {
+    this.router.navigate(["edit"], { relativeTo: this.route });
+    // this.router.navigate(['../', this.id, 'edit'], {relativeTo: this.route});
+  }
+}
